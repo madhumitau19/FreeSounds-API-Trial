28308437b7807ac069a2a18769d8b30101510007
madhumitau19 <madhumitau19@gmail.com>
2026-04-09 14:25:40 -0400

    debug

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ searchBtn.addEventListener('click', search);
 async function search() {
     const query = searchInput.value
     const apiKey = '7qRv74PMMNRaruCgFKr6hK3YdzJbOIwub0vPMPqv'
-    const url = `https://freesound.org/apiv2/search/text/?query=${query}&token=${apiKey}`
+    const url = `https://freesound.org/apiv2/search/text/?query=${query}&fields=id,name,previews&token=${apiKey}`
+
 
     const response = await fetch(url)
     const data = await response.json()
@@ -18,7 +19,7 @@ async function search() {
     data.results.forEach(sound => { 
 
         if (!sound.previews) return
-        
+
         const div = document.createElement('div')
         div.innerHTML = `
             <p>${sound.name}</p>
